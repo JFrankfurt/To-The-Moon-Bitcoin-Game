842e3fe632d778161409621bd267b64e92c2386b
JFrankfurt <generalj10@gmail.com>
2014-12-26 18:07:48 -0500

    change title from "phaser-example" to "ToTheMoon"

--- js/angular/app.js
+++ js/angular/app.js
@@ -36,7 +36,7 @@ app.controller('coinController', function($scope, makeCall) {
     $scope.cashOut = function() {
         makeCall.sendCoins($scope.address, $scope.score);
     };
-var game = new Phaser.Game(800, 600, Phaser.CANVAS,'phaser-example',
+var game = new Phaser.Game(800, 600, Phaser.CANVAS,'ToTheMoon',
                            { preload: preload,
                             create: create,
                             update: update,
