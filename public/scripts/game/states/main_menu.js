6732926d3c957e3c05e7440d7abdbd1b2cc6997a
ryskiz <ryan.skinner1193@gmail.com>
2015-01-31 18:59:24 -0500

    first crack at menu state

--- public/scripts/game/states/main_menu.js
+++ public/scripts/game/states/main_menu.js
@@ -2,6 +2,9 @@ function Mainmenu(game){}
 
 Mainmenu.prototype = {
     create: function(){
+        var playButton = this.game.add.button(300, 300, "Start", this.startGame, this);
+    },
+    startGame: function () {
         this.game.state.start("Play");
     }
 };
