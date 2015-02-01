4a99270226f1fdf7deead2960ecc2e40fe98741a
JFrankfurt <generalj10@gmail.com>
2015-02-01 16:09:01 -0500

    remove old alien reference

--- public/scripts/game/states/boot.js
+++ public/scripts/game/states/boot.js
@@ -3,7 +3,7 @@ function Boot(game){
 
 Boot.prototype = {
     preload: function(){
-        this.game.load.image("loading","assets/aliens-from-outer-space.png");
+
     },
     create: function(){
         this.game.state.start("Preload");
