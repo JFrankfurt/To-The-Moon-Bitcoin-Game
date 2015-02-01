9ff8d916ac920712f57b2b26b49b1cf801b78681
ryskiz <ryan.skinner1193@gmail.com>
2015-02-01 15:25:58 -0500

    no mas  scaling

--- public/scripts/game/states/boot.js
+++ public/scripts/game/states/boot.js
@@ -6,9 +6,8 @@ Boot.prototype = {
         this.game.load.image("loading","assets/aliens-from-outer-space.png");
     },
     create: function(){
-        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-        this.scale.pageAlignHorizontally = true;
-        this.scale.setScreenSize();
         this.game.state.start("Preload");
     }
 };
+
+
--- public/scripts/game/states/main_menu.js
+++ public/scripts/game/states/main_menu.js
@@ -1,8 +1,17 @@
 function Mainmenu(game){}
-
+var stateText;
 Mainmenu.prototype = {
     create: function(){
+        stateText = this.game.add.text(this.game.world.centerX, this.game.world.centerY, ' ', {
+            font: '84px Arial',
+            fill: '#fff'
+        });
         var playButton = this.game.add.button(300, 300, "Start", this.startGame, this);
+        this.title();
+    },
+    title: function () {
+        stateText.text = "To the Moon!";
+        stateText.visible = true;
     },
     startGame: function () {
         this.game.state.start("Play");
