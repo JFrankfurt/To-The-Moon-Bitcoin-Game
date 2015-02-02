d3b04fa9361290384dc468341b1db199e44eb2bf
ryskiz <ryan.skinner1193@gmail.com>
2015-02-02 01:03:13 -0500

    j commits

--- index.js
+++ index.js
@@ -20,28 +20,42 @@ if (cluster.isMaster) {
 } else {
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
-
     router.use(function(req, res, next){
         console.log("we got a request...");
         next();
     });
+    router.route('/endgame/:address')
+        .get(function(req, res) {
+            request.get('http://coinding.com/bitcoin/address/' + req.params.address, function(err, data){
+                if (err) {
+                    res.send(err);
+                    } else {
+                    res.json(data);
+                    }
+                }
+            )
+        });
+    router.route('/endgame/:address/:amount')
+        .get(function(req, res) {
+            var params = {
+                url: 'https://blockchain.info/merchant/a5f71e9f-d46f-439a-b418-cb6f83f972d3',
+                password: 'MoonBase GameFace',
+                to: request.params.address,
+                amount: request.params.amount,
+                from: '1Ke6KNxyai5Pa1ffnumFCtsQhy21yZC2wZ'
+            };
+
+            var please = qs.stringify(params);
+            request.get(please, function(err, data) {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.json(data);
+                }
+            });
+        });
+
 
     app.use(express.static(__dirname + '/public'));
-    app.use(function(req, res, next) {
-    });
-    app.get('/balance', function(request, response) {
-
-    });
-    app.get('/endGame/:address', function(request, response){
-            //respond with the number of satoshis this wallet has claimed from our service (currently just does balance)
-        var query = qs.stringify(request.param("address"));
-
-        next();
-    });
-    app.post('/endGame/:address/:ammount', function(request, response) {
-        response.send(
-            //alert that cashout was successful or failed (minimum not met, address not real)
-        );
-    });
     app.listen(process.env.PORT || 3000);
 }
