28b92801707094f40cc2a09d940ad09416c3487e
JFrankfurt <generalj10@gmail.com>
2015-02-06 18:07:45 -0500

    add helmet to dependencies and goof around a bit with request module

--- index.js
+++ index.js
@@ -23,30 +23,27 @@ if (cluster.isMaster) {
         cluster.fork();
     });
 } else {
-    app.use(helmet.hidePoweredBy({
-        setTo: "PHP 1.2.0"
-    }));
-/*  app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(bodyParser.json());*/
+    app.use(helmet.hidePoweredBy({setTo: "LOL"}));
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+
+    //confirm the request was received;
     router.use(function(req, res, next){
         console.log("we got a request...");
         next();
     });
     //should accept requests from mount directive
     router.route('/:address')
-        .get(function(req, res) {
+        .post(function(req, res) {
             console.log("mount request worked");
-            request.get('http://api.coinding.com/bitcoin/address/' + req.params.address, function(err, response, body) {
-                if (err) {
-                    res.send(err);
-                } else {
-                    res.json(body);
-                }
-            })
+            request.get('http://api.coinding.com/bitcoin/address/' + req.params.address)
+                .on('response', function(response) {
+                    console.log(response.statusCode)
+                }).pipe(request.put(''))
         });
-    //should accept requests from cashout directive
+    //should accept requests from cash out directive
     router.route('/:address/:amount')
-        .get(function(req, res) {
+        .post(function(req, res) {
             var params = {
                 url: 'https://blockchain.info/merchant/a5f71e9f-d46f-439a-b418-cb6f83f972d3',
                 password: 'MoonBase GameFace',
