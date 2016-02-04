7c1b420f19b0285861cd4283ef6de11d2da47866
Phillip Chaffee <phillipdensmorechaffee@gmail.com>
2016-02-04 03:53:37 +0000

    Angular Work
    -Wire up the about page state

--- public/scripts/MoonBaseApp.js
+++ public/scripts/MoonBaseApp.js
@@ -4,11 +4,18 @@ var moonbase = angular.module("moonbase", ["ui.router", "app.game"])
 
   $urlRouterProvider.otherwise('/index');
 
-  $stateProvider.state('index', {
+  $stateProvider
+
+    .state('index', {
     url: '/index',
     templateUrl: 'partials/main.html',
     controller: 'MenuController as menuCtrl'
-  });
+  })
+
+    .state('about', {
+      url: '/about',
+      templateUrl: 'partials/about.html'
+    });
 })
 
 .controller('MenuController', function($scope, $rootScope, gameFactory) {
